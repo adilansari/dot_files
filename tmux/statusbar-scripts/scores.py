0d0f7a92ba84bafd5242c4b89ec2eb3ea2ed690e
Adil Ansari <adil@tigrisdata.com>
2023-05-03 18:57:22 -0700

    feat: Soccer scores fixed

--- tmux/statusbar-scripts/scores.py
+++ tmux/statusbar-scripts/scores.py
@@ -1,9 +1,9 @@
 from abc import ABCMeta, abstractmethod
 from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 
 import requests as req
 import random
-import pytz
 
 
 class ScoresAbstract:
@@ -24,7 +24,8 @@ class ScoresAbstract:
 
 class CricketScores(ScoresAbstract):
     URL = 'https://hs-consumer-api.espncricinfo.com/v1/pages/matches/live?lang=en'
-    TEAM_KEYWORDS = ['INDIA', 'SA', 'AUS', 'PAK', 'NZ', 'ENG', 'BAN', 'WI', 'MI', 'RCB', 'CSK', 'PBKS', 'LSG', 'KKR']  # short_name
+    TEAM_KEYWORDS = ['INDIA', 'SA', 'AUS', 'PAK', 'NZ', 'ENG', 'BAN', 'WI', 'MI', 'RCB', 'CSK', 'PBKS', 'LSG',
+                     'KKR']  # short_name
 
     def _fetch_and_parse(self):
         resp = req.get(CricketScores.URL)
@@ -35,7 +36,7 @@ class CricketScores(ScoresAbstract):
 
     def _process_data(self, matches):
         self._set_random_match(matches)
-        #individual_scores = self._get_individual_scores(self.match)
+        # individual_scores = self._get_individual_scores(self.match)
         return '{} || {}'.format(self._get_team_display(), self._get_score_text())
 
     def _get_score_text(self):
@@ -59,7 +60,8 @@ class CricketScores(ScoresAbstract):
         else:
             return 'AlgorithmError: No teams live'
 
-        batting_score, bowling_score = '{} in {}'.format(batting_team['score'], batting_team['scoreInfo']), bowling_team['score']
+        batting_score, bowling_score = '{} in {}'.format(batting_team['score'], batting_team['scoreInfo']), \
+            bowling_team['score']
         batting_formatted_score = '{}: {}'.format(batting_team['team']['abbreviation'], batting_score)
         bowling_formatted_score = '{}: {}'.format(bowling_team['team']['abbreviation'], bowling_score)
 
@@ -102,31 +104,37 @@ class SoccerScores(ScoresAbstract):
     """
     Register at http://football-data.org/ for API key
     """
-
-    URL = 'http://api.football-data.org/v2/matches'
+    URL = 'https://www.fotmob.com/api/matches'
     DEFAULT_SCORELINE = 'A.R.S.E.N.A.L'
     TEAMS = {
-        57: 'Arsenal',
-        61: 'Chelsea',
-        65: 'ManCity',
-        66: 'ManUtd',
-        73: 'Tottenham',
-        81: 'Barcelona',
-        86: 'Real Madrid',
-        119: 'Juventus',
-        524: 'PSG',
-        232: 'Bayern',
+        9825: 'Arsenal',
+        8455: 'Chelsea',
+        8456: 'Man City',
+        10260: 'Man Utd',
+        8586: 'Tottenham',
+        8650: 'Liverpool',
+        8634: 'Barcelona',
+        8633: 'Real Madrid',
+        9885: 'Juventus',
+        9847: 'PSG',
+        9823: 'Bayern',
+        6603: 'SJC Quakes'
     }
     COMPETITIONS = {
-        'Premier League': '2021',
-        'Bundesliga': '2002',
-        'Italian Serie A': '2019',
-        'Champions League': '2001',
-        'Europa League': '2046',
+        47: 'Premier League',
+        87: 'Spanish La Liga',
+        53: 'French Ligue 1',
+        55: 'Serie A',
+        42: 'Champions League',
+        54: 'Bundesliga',
+        10000002: 'MLS'
     }
+    TZ = ZoneInfo('America/Los_Angeles')
 
-    def __init__(self, _api_key):
-        self.headers = {'X-Auth-Token': _api_key, 'X-Response-Control': 'minified'}
+    def __init__(self):
+        dt = datetime.now(tz=SoccerScores.TZ) + timedelta(days=random.randint(-1, 3))
+        resp = req.get(SoccerScores.URL, params={'date': dt.strftime('%Y%m%d'), 'timezone': SoccerScores.TZ.key})
+        self.matches = self.validate_response(resp, self.response_callback)
 
     def get_display_string(self):
         try:
@@ -135,55 +143,48 @@ class SoccerScores(ScoresAbstract):
             return SoccerScores.DEFAULT_SCORELINE
 
     def _get_fixtures(self):
-        start_date, end_date = self.get_start_end_dates()
-        payload= {
-            'dateFrom': start_date,
-            'dateTo': end_date,
-            'competitions': ','.join(SoccerScores.COMPETITIONS.values())
-        }
+        self.match = random.choice(self.matches)
+        home_team, away_team = self.match['home']['name'], self.match['away']['name']
 
-        resp = req.get(SoccerScores.URL, params=payload, headers=self.headers)
-        matches = filter(SoccerScores._matches_filter, self.validate_response(resp, self.response_callback))
-        match = random.choice(matches)
-        home_team, away_team = match['homeTeam']['name'], match['awayTeam']['name']
-        home_team_score, away_team_score = match['score']['fullTime']['homeTeam'] or 0, match['score']['fullTime'][
-            'awayTeam'] or 0
-        fixture_date = self.get_pdt_date(match['utcDate'])
+        return '{} {} {} || {}'.format(home_team, self._get_match_score(), away_team, self._get_match_status())
 
-        return '{}  {}:{}  {} || {}'.format(home_team, home_team_score, away_team_score, away_team, fixture_date)
+    def _get_match_score(self):
+        match_status = self.match['status']
+        if not match_status.get('started', False):
+            return "vs."
 
-    @staticmethod
-    def _matches_filter(match):
-        if match['awayTeam']['id'] in SoccerScores.TEAMS.keys():
-            return True
-        elif match['homeTeam']['id'] in SoccerScores.TEAMS.keys():
-            return True
-        return False
+        return match_status['scoreStr']
 
-    @staticmethod
-    def get_start_end_dates():
-        start_date = datetime.now() - timedelta(days=2)
-        end_date = start_date + timedelta(days=8)
-        return start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')
-
-    @staticmethod
-    def get_pdt_date(date_string):
-        dt = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=pytz.utc)
-        return dt.astimezone(pytz.timezone('US/Pacific')).strftime('%Y-%m-%d %H:%M:%S %Z')
+    def _get_match_status(self):
+        match_status = self.match['status']
+        if match_status.get('finished'):
+            return 'FT'
+        if match_status.get('liveTime'):
+            return match_status['liveTime']['short']
+        utc_time = match_status['utcTime'].replace('Z', '+00:00')  # "2023-05-03T19:00:00.000Z"
+        start_time = datetime.fromisoformat(utc_time)
+        local_time = start_time.astimezone(SoccerScores.TZ)
+        return local_time.strftime('%I:%M %p')
 
     def validate_response(self, response, callback):
         return super(SoccerScores, self).validate_response(response, callback)
 
     def response_callback(self, data):
-        return data['matches']
+        matches = []
+        for league in data['leagues']:
+            if league['id'] in SoccerScores.COMPETITIONS.keys():
+                for match in league['matches']:
+                    for k in ['home', 'away']:
+                        if match[k]['id'] in SoccerScores.TEAMS.keys():
+                            matches.append(match)
+        return matches
 
 
 if __name__ == '__main__':
-    # if random.randint(1, 2) == 1:
-    score = CricketScores()
-    # else:
-    #     score = SoccerScores(sys.argv[1])
-    # score = SoccerScores(sys.argv[1])
+    if random.randint(1, 100) < 10:
+        score = CricketScores()
+    else:
+        score = SoccerScores()
     score_display = score.get_display_string()
 
     if not score_display:
