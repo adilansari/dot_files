2ed6aa2d6a78cd6baeb85e295d5289cbf256170f
Adil Ansari <adil@tigrisdata.com>
2024-11-28 12:54:38 -0800

    updating symlink script

--- install.sh
+++ install.sh
@@ -7,35 +7,37 @@ if [[ ! -d $DOT_FILES_DIR ]]; then
   exit 1
 fi
 
-# following are file or directory names under which files should be looked into
-filenames=(
-	'zsh'
-	'vim'
-	'tmux'
-	'git'
-	'.bash_profile'
-	'config'
-) 
+# following are file or directory names to create symlinks
+# by default, all symlinks are created to $HOME
+# but you can specify a different target using colon : delimiter
+linktargets=(
+  'zsh/.zsh'
+  'zsh/.zshrc'
+  'vim/.vimrc'
+  'tmux/.tmux.conf'
+  'tmux/.tmuxinator'
+  'git/.git-completion.bash'
+  'git/.gitconfig'
+  'git/.gitignore'
+  '.bash_profile'
+  'config/gh-dash:$HOME/.config/gh-dash'
+)
 
 # Create symlinks for dot_files
 # ln -sf source_file target_file
 create_symlink() {
-	echo "creating symlink for $1 to $HOME"
-	ln -sf $1 $HOME
+	echo "creating symlink for $1 to $2"
+	ln -sf $1 $2
 }
 
 # iterate over given file/directory names
-for name in ${filenames[@]}; do
-	path=$DOT_FILES_DIR/$name
-	if [[ -d $path ]]; then
-		for f in $path/.*; do
-			create_symlink $f
-		done
-	elif [[ -f $path ]]; then
-		create_symlink $path
-	else
-		echo "invalid filename"
-	fi
+for item in ${linktargets[@]}; do
+  source=${item%%:*}
+  target=${item#*:}
+  if [[ -z "$target" || "$target" = "$source" ]]; then
+    target=$HOME
+  fi
+  create_symlink $DOT_FILES_DIR/$source $target
 done
 
 exit 0
